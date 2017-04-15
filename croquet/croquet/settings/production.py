c99a447947ba63664f980c18e678ee96331f3968
Paul Rudin <paul@rudin.co.uk>
2017-04-15 16:40:10 +0100

    sort production settings/config

--- croquet/croquet/settings/production.py
+++ croquet/croquet/settings/production.py
@@ -2,6 +2,8 @@ import os
 
 from .base import *
 
+ALLOWED_HOSTS = ['histoncroquet.org', 'localhost']
+
 DEBUG = False
 
 SECRET_KEY = os.getenv('SECRET_KEY')
--- croquet/croquet/wsgi.py
+++ croquet/croquet/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "croquet.settings.dev")
+os.environ["DJANGO_SETTINGS_MODULE"] = "croquet.settings.production"
 
 application = get_wsgi_application()
